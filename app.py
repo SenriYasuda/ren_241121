98629af16d20eb89c9ef84d5e69552f2985e27dd
SenriYasuda <ysd1000ri@gmail.com>
2024-11-21 23:53:40 +0900

    fiveth commit

--- app.py
+++ app.py
@@ -17,36 +17,19 @@ def allowed_file(filename):
 # ゲーム結果を受け取るAPI
 @app.route('/upload_result', methods=['POST'])
 def upload_result():
-    # ファイルがリクエストに含まれていない場合
     if 'file' not in request.files:
-        return jsonify({"error": "No file_mywrite"}), 400
-    
+        return jsonify({"error": "No file part"}), 400
     file = request.files['file']
-    
-    # ファイルが選択されていない場合
     if file.filename == '':
-        return jsonify({"error": "No file_mywrite"}), 400
-    
-    # 有効なファイル形式の場合
+        return jsonify({"error": "No selected file"}), 400
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-
-        # アップロード先のフォルダがない場合は作成
-        if not os.path.exists(app.config['UPLOAD_FOLDER']):
-            os.makedirs(app.config['UPLOAD_FOLDER'])
-        
-        # ファイルを保存
         file.save(filepath)
-
-        # 画像は10分後に削除
-        time.sleep(600)  # 10分
+        time.sleep(600)  # 画像は10分後に削除
         os.remove(filepath)
-
         return jsonify({"message": "File uploaded successfully"}), 200
-    
-    # 無効なファイル形式の場合
-    return jsonify({"error": "無効なファイル形式"}), 400
+    return jsonify({"error": "Invalid file format"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
